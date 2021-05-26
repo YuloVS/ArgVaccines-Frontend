1a5fba14d4cb579ab741b7a390dc8266c5ab25cf
YuloVS <63474125+YuloVS@users.noreply.github.com>
2021-05-25 21:04:14 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:YuloVS/ArgVaccines-Frontend.git main:gh-pages
+git push -f git@github.com:YuloVS/ArgVaccines-Frontend.git master:gh-pages
 
 cd -
