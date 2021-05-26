397de41f3e6c295c53dc664f1f8a50b746edaf1e
YuloVS <63474125+YuloVS@users.noreply.github.com>
2021-05-26 11:28:45 -0300

    Remove unused packages

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import axios from "axios";
-import VCharts from 'v-charts'
 import echarts from "echarts"
 
-Vue.use(VCharts)
 Vue.prototype.$echarts = echarts;
 //Vue.prototype.$axios = axios;
 
